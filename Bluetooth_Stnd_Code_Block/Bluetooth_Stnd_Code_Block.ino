de6a644a70df438771e85078ccc89dca420bb098
Mark Gryzwa <markgryzwa@gmail.com>
2018-11-30 14:13:18 -0600

    Add files via upload

---
+++ Bluetooth_Stnd_Code_Block/Bluetooth_Stnd_Code_Block.ino
@@ -0,0 +1,21 @@
+
+
+<#include SoftwareSerial.h>
+SoftwareSerial BTSerial(2,3)    // (RX,TX) pins
+char charDataIn = 0;
+// ----------------------------------------------------------------------
+
+void setup() {
+  BTSerial.begin(38400);   // my HC-06 wants to communicate at 38,400 
+
+}
+// ----------------------------------------------------------------------
+
+void loop() {
+  if(BTSerial.available()>0){
+    charDataIn = BTSerial.read();
+
+    // do your thing with new serial data
+  }
+
+}
---
+++ LCD_Stnd_Code_Block/LCD_Stnd_Code_Block.ino
@@ -0,0 +1,21 @@
+
+#include <Wire.h> 
+#include <LiquidCrystal_I2C.h>
+LiquidCrystal_I2C lcd(0x3f,20,4);  // set the LCD address to 0x3f (address,x,y)
+                                   // its not at 0x27 as internet instructions state
+                                   // connect SDA to A4 and SCL to A5
+//  ----------------------------------------------------------------------------------
+
+void setup() {
+  lcd.init();                      // initialize the lcd 
+  lcd.backlight();                 
+  lcd.clear();
+
+}
+//  ----------------------------------------------------------------------------------
+
+void loop() {
+  lcd.setCursor(x,y);
+  lcd.print("Hello>");
+
+}
